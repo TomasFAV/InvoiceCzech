ca4d9d507e260051c0941948d285436f8c904c3d
tomík <brabect@gapps.zcu.cz>
2026-03-21 21:46:33 +0100

    micro and macro NED

--- evaluation_utils/utils.py
+++ evaluation_utils/utils.py
@@ -607,7 +607,7 @@ def compute_metrics(preds: List[dict], answers: List[dict]):
 
   mean_acc = 0.0
   mean_f1 = 0.0 
-  mean_f1_05 = 0.0
+  mean_f1_1 = 0.0
   mean_ned = 0.0
 
   f1_standard_deviation = 0.0
