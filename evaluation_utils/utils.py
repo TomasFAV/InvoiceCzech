d50711d5d61a1fc74c4d5b786383aaa463b79704
tomík <brabect@gapps.zcu.cz>
2026-04-19 20:09:04 +0200

    Evalutaion edited

--- evaluation_utils/utils.py
+++ evaluation_utils/utils.py
@@ -645,7 +645,7 @@ def compute_metrics(preds: List[dict], answers: List[dict], file_names: List[str
   return {"micro F1": micro_f1, "macro F1": mean_f1, "macro F1 min": numpy.min(f1_scores), "macro-f1-dev": f1_standard_deviation,
           "macro-f1-P50": f1_P50, "macro-f1-P25": f1_P25, "macro-f1-P05": f1_P05, "accuracy": mean_acc, "document_exact_match": document_exact_match,
           "mean-field-NED": micro_ned,"macro-ned":mean_ned, "worst_extractions": documents_f1s[:min(3, len(documents_f1s))],
-          "average_extractions": average_extractions, "best_extractions": documents_f1s[:-(min(3, len(documents_f1s)))]}
+          "average_extractions": average_extractions, "best_extractions": documents_f1s[-(min(3, len(documents_f1s))):]}
 
 
 from enum import Enum
